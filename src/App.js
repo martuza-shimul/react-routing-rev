22686618118e793a28b797e0f71d7a3f9815f360
Martuza Shimul <martuza.shimul@gmail.com>
2019-10-22 11:29:31 +0600

    route parameter added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Post from './components/Post'
 
 export class App extends Component {
   render() {
@@ -13,7 +14,8 @@ export class App extends Component {
           <Navbar />
           <Route path = "/" exact component = {Home} />
           <Route path = "/about" component = {About} />
-          <Route path = "/contact" component = {Contact} />
+          <Route path = "/contact"  component = {Contact} />
+          <Route path = '/:post_id'  component={Post} />
         </div>
       </Router>
     );
