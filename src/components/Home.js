5fbe8fe1a1158d57a3607a0bae1b2ac5d73b2ae3
Martuza Shimul <martuza.shimul@gmail.com>
2019-10-22 11:09:49 +0600

    data fetched through axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">This is the Home-Page</h4>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo rem repellat aperiam omnis
-        rerum est quisquam harum ad neque officia.
-      </p>
-    </div>
-  );
-};
+class Home extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+      // console.log(res);
+      this.setState({
+        posts: res.data.slice(0, 10)
+      });
+    });
+  }
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div>No post yet</div>
+    );
+    return (
+      <div className="container">
+        <h4 className="center">Home-Page</h4>
+        {postList}
+      </div>
+    );
+  }
+}
 export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link, NavLink, withRouter } from 'react-router-dom'; //? withRouter is an higher-order component
+import { Link, NavLink } from 'react-router-dom';
+// import { Link, NavLink, withRouter } from 'react-router-dom'; //? withRouter is an higher-order component
 
 const Navbar = (props) => {
 
