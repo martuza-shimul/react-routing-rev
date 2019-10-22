f9c8dc343be26bbe820cd57fe43a51efb9b3b096
Martuza Shimul <martuza.shimul@gmail.com>
2019-10-22 10:06:55 +0600

    higher-order component initialized

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, withRouter } from 'react-router-dom'; //? withRouter is an higher-order component
 
 const Navbar = () => {
   return (
