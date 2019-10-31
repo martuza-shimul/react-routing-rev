45a443c0ea87fb8ef6e296885bcd13a9b644ee9f
Martuza Shimul <martuza.shimul@gmail.com>
2019-10-31 22:43:39 +0600

    delete post added

--- src/components/Post.js
+++ src/components/Post.js
@@ -17,11 +17,21 @@ class Post extends Component {
   //         })
 
   // }
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push("/");
+  };
   render() {
+    console.log(this.props);
     const post = this.props.post ? (
       <div className="post">
         <h4 className="center">{this.props.post.title}</h4>
         <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>
+            Delete Post
+          </button>
+        </div>
       </div>
     ) : (
       <div className="center"> Loading Post...</div>
@@ -38,4 +48,15 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps)(Post);
+const mapDisptchToProps = dispatch => {
+  return {
+    deletePost: id => {
+      dispatch({ type: "DELETE_POST", id: id });
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDisptchToProps
+)(Post);
